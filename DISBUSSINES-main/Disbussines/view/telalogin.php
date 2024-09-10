a90094ed4d90bebce1f5a1c74fb0b5a33f066725
ana1234455 <analaura.estudos18@gmail.com>
2024-09-10 15:36:11 -0300

    Add files via upload

---
+++ DISBUSSINES-main/Disbussines/view/telalogin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/stylelogin.css">
+    <title>DisBusiness - LOGIN</title>
+</head>
+
+<body>
+    <div class="principal">
+        <!-- //main-login -->
+        <div class="antes-login">
+            <!-- //left-login -->
+            <h1>Faça login<br> E comece agora a divulgar e conhecer microempresas!
+            </h1>
+            <img src="../imagens/business.svg" alt="Empresa" class="antes-login-animacao">
+        </div>
+        <div class="depois-login">
+            <!-- //right-login -->
+            <div class="carta-login">
+                <!-- //card-login -->
+                <h1>LOGIN</h1>
+                <div class="text-field">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="usuario">
+                </div>
+                <div class="text-field">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="senha">
+                </div>
+
+                <input type="submit" class="btn-login"></input>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
